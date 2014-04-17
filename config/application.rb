e61bcd2f34ebdf0c5548a0cfc353149e7ab7e688
Leonardo Saraiva <vyper@maneh.org>
2014-04-17 01:04:34 +0000

    Replaced gem for tests

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Bolao
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Test framework
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
