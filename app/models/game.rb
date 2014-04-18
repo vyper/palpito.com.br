09006ce46ad19e851ac3df53509415ba042964db
Leonardo Saraiva <vyper@maneh.org>
2014-04-18 02:53:47 +0000

    Commented validations because rails_admin incompatibility

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,8 +9,9 @@ class Game < ActiveRecord::Base
   validates :team_home, presence: true
   validates :team_away, presence: true
   validates :round,     presence: true
-  validates :team_home_goals, numericality: { greater_than: -1 }
-  validates :team_away_goals, numericality: { greater_than: -1 }
+  # TODO: enable this validates after fix in rails_admin
+  # validates :team_home_goals, numericality: { greater_than: -1 }
+  # validates :team_away_goals, numericality: { greater_than: -1 }
 
   ## methods
   def played?
