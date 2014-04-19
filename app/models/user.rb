6b237d82564a9d38f9295dcfce9d9e994205f952
Leonardo Saraiva <vyper@maneh.org>
2014-04-19 02:18:21 +0000

    Enable devise invitable module in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
 
   ## validations
   validates :nickname,   presence: true, uniqueness: true, length: { minimum: 2, maximum: 15 }
