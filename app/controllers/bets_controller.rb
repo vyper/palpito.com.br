64c79e88f0183e772681f163c6e4b1f764995f65
Leonardo Saraiva <vyper@maneh.org>
2014-04-21 16:13:47 +0000

    Added group and week navigation

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -4,8 +4,35 @@ class BetsController < ApplicationController
   respond_to :html
 
   def index
-    @bets = current_user.bets
+    @groups  = current_user.groups.order(:name)
+    @members = group.members
+
+    @bets = current_user.bets.
+              joins(game: :round).
+              where(
+                rounds: { championship_id: group.championship_id },
+                games: { played_at: week.to_range }
+              ).
+              order('"games"."played_at" ASC')
 
     respond_with @bets
+  end
+
+private
+  def week
+    @week = Week.new(group.championship, params[:week])
+  end
+
+  def group
+    if params[:group_id].present?
+      @group = @groups.find(params[:group_id])
+      session[:group_id] = params[:group_id]
+    elsif session[:group_id].present?
+      @group = @groups.find(session[:group_id])
+    else
+      @group = @groups.first
+    end
+
+    @group
   end
 end
