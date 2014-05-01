c672ac6fe36239e5b4d9d43111e7ee62f021ca3d
Leonardo Saraiva <vyper@maneh.org>
2014-05-01 21:16:32 +0000

    Added implementation base to auth using fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable, :invitable
+         :recoverable, :rememberable, :trackable, :validatable, :invitable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   ## validations
   validates :nickname,   presence: true, uniqueness: true, length: { minimum: 2, maximum: 15 }
@@ -28,5 +29,41 @@ class User < ActiveRecord::Base
 
   def to_s
     nickname
+  end
+
+  def self.find_for_facebook_oauth(auth)
+    binding.pry
+    params = auth.slice(:provider, :uid).merge(email: auth.info.email)
+    user = where("email = :email OR (provider = :provider AND uid = :uid)", params).first_or_create do |u|
+      u.provider   = auth.provider
+      u.uid        = auth.uid
+      u.email      = auth.info.email
+      u.password   = Devise.friendly_token[0,20]
+      u.first_name = auth.info.first_name
+      u.last_name  = auth.info.last_name
+    end
+    # # TODO improve this
+    # user = where(email: auth.info.email).first
+    # user = where(provider: auth.provider, uid: auth.uid).first unless user
+
+    # unless user
+    #   user = User.create provider: auth.provider,
+    #                      uid: auth.uid,
+    #                      email: auth.info.email,
+    #                      name: auth.extra.raw_info.name,
+    #                      password: Devise.friendly_token[0,20]
+    # end
+
+    # user
+
+    # where(auth.slice(:provider, :uid)).first_or_create do |user|
+    #   user.provider = auth.provider
+    #   user.uid = auth.uid
+    #   user.email = auth.info.email
+    #   user.password = Devise.friendly_token[0,20]
+    #   binding.pry
+    #   user.first_name = auth.info.first_name
+    #   user.last_name  = auth.info.last_name
+    # end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,6 +269,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1427589860827129", "4133ad68466a5400d0952be8a9540d2b"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
