661faf37870e2c0e9d3e6b63a02e8be6e2855c31
Leonardo Saraiva <vyper@maneh.org>
2014-05-02 01:18:22 +0000

    Added 'is_bettable' to bet serializer

--- spec/serializers/bet_serializer_spec.rb
+++ spec/serializers/bet_serializer_spec.rb
@@ -16,5 +16,5 @@ describe BetSerializer do
   it { expect(subject.team_away).to eq game.team_away.to_s }
   it { expect(subject.team_away_image_url).to eq game.team_away.image.url }
 
-  it { expect(subject.as_json).to eq({ id: 628721556, team_home_goals: 1, team_away_goals: 0, team_home: "São Paulo", team_away: "Paraná", round: "1ª", team_home_image_url: "/system/teams/images/136/128/820/original/spec/support/test.png", team_away_image_url: "/system/teams/images/694/025/141/original/spec/support/test.png", weekday: "Qua", time: "15:00", date: "01/01" }) }
+  it { expect(subject.as_json).to eq({ id: 628721556, team_home_goals: 1, team_away_goals: 0, points: nil, is_bettable: false, team_home: "São Paulo", team_away: "Paraná", round: "1ª", team_home_image_url: "/system/teams/images/136/128/820/original/spec/support/test.png", team_away_image_url: "/system/teams/images/694/025/141/original/spec/support/test.png", weekday: "Qua", time: "15:00", date: "01/01" }) }
 end
