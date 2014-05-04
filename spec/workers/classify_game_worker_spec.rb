2fefb32d419bd6de2e3acd164a5723467c065369
Leonardo Saraiva <vyper@maneh.org>
2014-05-04 16:15:59 +0000

    Added specs for ClassifyGameWorker

--- spec/workers/classify_game_worker_spec.rb
+++ spec/workers/classify_game_worker_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe ClassifyGameWorker do
-  pending '#perform'
+  fixtures :games, :bets
+
+  it '#perform' do
+    ClassifyBetWorker.should_receive(:perform_async).with(bets(:sao_w_x_par).id).once
+    subject.perform games(:sao_x_par).id
+  end
 end
