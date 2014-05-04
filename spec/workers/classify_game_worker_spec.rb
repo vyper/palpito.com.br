88b1ea89dbdd84ba330d96cc30a1ae45c9ae168f
Leonardo Saraiva <vyper@maneh.org>
2014-05-04 19:15:37 +0000

    FIXED: Upgrade spec syntax from 'should' to 'expect'

--- spec/workers/classify_game_worker_spec.rb
+++ spec/workers/classify_game_worker_spec.rb
@@ -4,7 +4,7 @@ describe ClassifyGameWorker do
   fixtures :games, :bets
 
   it '#perform' do
-    ClassifyBetWorker.should_receive(:perform_async).with(bets(:sao_w_x_par).id).once
+    expect(ClassifyBetWorker).to receive(:perform_async).with(bets(:sao_w_x_par).id).once
     subject.perform games(:sao_x_par).id
   end
 end
