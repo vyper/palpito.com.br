47d798e446ee0b1775d04d6695ee9e3549ca5b54
Leonardo Saraiva <vyper@maneh.org>
2014-05-04 16:48:18 +0000

    Added specs for ClassifyBetWorker

--- spec/workers/classify_bet_worker_spec.rb
+++ spec/workers/classify_bet_worker_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ClassifyBetWorker do
-  pending '#perform'
+  fixtures :bets
+
+  it '#perform' do
+    expect {
+      subject.perform(bets(:sao_w_x_par))
+    }.to change { Bet.find(bets(:sao_w_x_par).id).points }.from(nil).to(40)
+  end
 end
