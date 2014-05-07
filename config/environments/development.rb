acd0ff6431fc5cfd5e11a62687ed88b9eae7e7b5
Leonardo Saraiva <vyper@maneh.org>
2014-05-07 15:43:41 +0000

    Added configuration for bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Bullet
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
