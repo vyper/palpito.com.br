eed0df9aa3af265f87e9e8679f1406d9db467aa4
Leonardo Saraiva <vyper@maneh.org>
2014-05-23 00:02:38 +0000

    Added helpers for login in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   config.include Capybara::DSL
+  config.include Warden::Test::Helpers
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
