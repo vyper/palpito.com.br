5114b26a541a1b88f053d5017dd08c0bb9de075c
Leonardo Saraiva <vyper@maneh.org>
2014-05-26 16:30:24 +0000

    Added features specs for championship crud

---
+++ spec/features/championships_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'Championships' do
+  fixtures :users, :championships
+
+  before(:each) { login_as(users(:vyper), scope: :user, run_callbacks: false) }
+
+  it 'listing' do
+    visit '/championships'
+
+    expect(page).to have_content "Copa do Mundo"
+  end
+
+  it 'creating' do
+    visit '/championships/new'
+
+    fill_in 'championship_name',        with: 'Campeonato Brasileiro 2014'
+    fill_in 'championship_started_at',  with: -7.days.from_now
+    fill_in 'championship_finished_at', with: 7.days.from_now
+    click_on 'Salvar'
+
+    expect(page).to have_content 'Campeonato Brasileiro 2014'
+    expect(page).to have_content "Campeonato 'Campeonato Brasileiro 2014' salvo com sucesso"
+  end
+
+  it 'updating' do
+    visit "/championships/#{championships(:worldcup).id}/edit"
+
+    fill_in 'championship_name',        with: 'World Cup 2014'
+    fill_in 'championship_started_at',  with: -7.days.from_now
+    fill_in 'championship_finished_at', with: 7.days.from_now
+    click_on 'Salvar'
+
+    expect(page).not_to have_content 'Copa do Mundo 2014'
+    expect(page).to have_content 'World Cup 2014'
+    expect(page).to have_content "Campeonato 'World Cup 2014' salvo com sucesso"
+  end
+
+  it 'destroying' # do
+  #   visit '/championships'
+  #   expect {
+  #     all("a:last[data-method='delete']").last.click
+  #   }.to change(Championship, :count).by(-1)
+  # end
+end
