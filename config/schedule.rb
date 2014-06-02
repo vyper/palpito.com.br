f94113abff31940cb86dd3f043c6170791a3d4ea
Leonardo Saraiva <vyper@maneh.org>
2014-06-02 16:46:02 +0000

    Added cron to backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,10 @@
+set :output, 'log/cron.log'
+job_type :bin,  "cd :path && :environment_variable=:environment bin/:task :output"
+
 every 1.hour do
   runner 'CreateBetsWorker.perform_async'
+end
+
+every :day do
+  bin "backup_db"
 end
