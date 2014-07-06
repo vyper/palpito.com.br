c768e6561fc741357cf972e8fd82dfb09ae8084f
Leonardo Saraiva <vyper@maneh.org>
2014-07-06 01:09:48 +0000

    Remove validation uniqueness on short_name of team

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ActiveRecord::Base
 
   ## validations
   validates :name,       presence: true, uniqueness: true, length: { maximum: 50, minimum: 3 }
-  validates :short_name, presence: true, uniqueness: true, length: { maximum:  3, minimum: 3 }
+  validates :short_name, presence: true, length: { maximum:  3, minimum: 3 }
 
   validates :image, attachment_presence: true,
                     attachment_content_type: { content_type: %w{ image/png image/gif image/jpg } }
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -11,7 +11,6 @@ describe Team do
   it { expect(subject).to ensure_length_of(:name).is_at_least(3).is_at_most(50) }
 
   it { expect(subject).to validate_presence_of(:short_name) }
-  it { expect(subject).to validate_uniqueness_of(:short_name) }
   it { expect(subject).to ensure_length_of(:short_name).is_at_least(3).is_at_most(3) }
 
   it { expect(subject).to validate_attachment_presence(:image) }
