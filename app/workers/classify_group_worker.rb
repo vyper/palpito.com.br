daeb0563c17276a5731bb9596258dacee777f256
Leonardo Saraiva <vyper@maneh.org>
2014-07-17 22:27:34 +0000

    FIXED: Ranking generator

--- app/workers/classify_group_worker.rb
+++ app/workers/classify_group_worker.rb
@@ -7,14 +7,14 @@ class ClassifyGroupWorker
     # TODO: simplify this query +_+
     members = group.members.
                 select("members.user_id, members.id, sum(bets.points) as points").
-                includes(:user).
-                joins(user: { bets: :game }).
+                joins(user: { bets: { game: :round } }).
+                joins(:group).
                 where("games.played_at < ?", DateTime.now.in_time_zone).
-                where("bets.points is not null").
+                where("rounds.championship_id = groups.championship_id").
                 group("members.user_id, members.id")
 
     members.each do |member|
-      Member.find(member.id).update points: member.points
+      Member.find(member.id).update points: member.points || 0
     end
   end
 end
