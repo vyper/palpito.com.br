7c9809e122011053d3c2fa43d4cccd78b0b3da86
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2014-08-13 16:33:45 +0000

    Remove update

--- db/migrate/20140812152605_add_championship_to_games.rb
+++ db/migrate/20140812152605_add_championship_to_games.rb
@@ -1,6 +1,5 @@
 class AddChampionshipToGames < ActiveRecord::Migration
   def change
     add_reference :games, :championship, index: true
-    Game.all.each { |game| game.update championship_id: game.round.championship_id }
   end
 end
