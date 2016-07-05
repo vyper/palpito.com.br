7095b360290e6072112e8f3b8327b69b273b968a
Leonardo Saraiva <vyper@maneh.org>
2016-07-05 13:52:48 -0300

    Now new bets are created with random goals

--- app/workers/create_bets_worker.rb
+++ app/workers/create_bets_worker.rb
@@ -2,15 +2,25 @@ class CreateBetsWorker
   include Sidekiq::Worker
 
   def perform
-    result = Game.select('"games"."id" as "game_id", "members"."user_id" as "user_id"').
+    rows = Game.select('"games"."id" as "game_id", "members"."user_id" as "user_id"').
                   joins(championship: { groups: :members }).
                   joins('LEFT JOIN "bets" ON "bets"."user_id" = "members"."user_id" and "bets"."game_id" = "games"."id"').
                   where(bets: { id: nil }).
                   group('"games"."id", "members"."user_id"')
 
-    result.each do |g|
-      bet = Bet.new game_id: g.game_id, user_id: g.user_id
-      bet.save
+    rows.each do |row|
+      Bet.create(
+        game_id: row.game_id,
+        user_id: row.user_id,
+        team_home_goals: sample_goal,
+        team_away_goals: sample_goal
+      )
     end
+  end
+
+  private
+
+  def sample_goal
+    [1, 2, 3, 4].sample
   end
 end
--- spec/workers/create_bets_worker_spec.rb
+++ spec/workers/create_bets_worker_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
-describe CreateBetsWorker do
-  pending '#perform'
+RSpec.describe CreateBetsWorker, type: :worker do
+  fixtures :games, :users, :members, :teams, :bets, :championships
+
+  describe '#perform' do
+    context 'user without bet in game' do
+      let(:team_home)    { teams(:sao) }
+      let(:team_away)    { teams(:cfc) }
+      let(:championship) { championships(:worldcup) }
+      let!(:new_game)    { Game.create(team_home: team_home, team_away: team_away, championship: championship, played_at: Date.tomorrow) }
+
+      it 'creates bet using random goals' do
+        subject.perform
+
+        last_bet = Bet.last
+        expect(last_bet.team_home_goals.in?([1,2,3,4])).to be_truthy
+        expect(last_bet.team_away_goals.in?([1,2,3,4])).to be_truthy
+      end
+
+      it 'creates bet for games without bet' do
+        expect {
+          subject.perform
+        }.to change(Bet, :count).by(1)
+      end
+    end
+
+    it 'does not create bet to user with bet in all games' do
+      expect {
+        subject.perform
+      }.to_not change(Bet, :count)
+    end
+  end
 end
