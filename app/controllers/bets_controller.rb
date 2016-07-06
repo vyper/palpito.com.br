54d0ec7e0ae3a19f93f6262b786d22d9d5c63756
Leonardo Saraiva <vyper@maneh.org>
2016-07-05 23:03:46 -0300

    Renamed from #before_filter to #before_action

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,6 +1,6 @@
 class BetsController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :with_groups?
+  before_action :authenticate_user!
+  before_action :with_groups?
 
   respond_to :html, :json
 
